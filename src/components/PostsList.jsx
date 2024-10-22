d60bd50e9dd7654f0c86f3afdd39c9cbc006dee7
lucassalicano <lucassalicano@hotmail.com>
2024-10-22 00:06:19 -0300

    feat: add state and conditional content

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,23 +1,42 @@
 import Post from "./Post"
 import NewPost from './NewPost';
-import classes from './PostsList.module.css';
 import { useState } from 'react';
+import Modal from './Modal';
+import classes from './PostsList.module.css';
 
 function PostsList() {
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
+    const [modalIsVisible, setModalIsVisible] = useState(true);
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
 
     function changeBodyHandler(event) {
-      setEnteredBody(event.target.value);
+        setEnteredBody(event.target.value);
     }
 
     function changeAuthorHandler(event) {
-      setEnteredAuthor(event.target.value);
+        setEnteredAuthor(event.target.value);
     }
-  
+
+    let modalContent;
+
+    if (modalIsVisible) {
+        modalContent = (
+            <Modal onClose={hideModalHandler}>
+                <NewPost
+                    onBodyChange={changeBodyHandler}
+                    onAuthorChange={changeAuthorHandler}
+                />
+            </Modal>
+        );
+    }
+
     return (
         <>
-            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} />
+            {modalContent}
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Lucas Salicano" body="body text two" />
